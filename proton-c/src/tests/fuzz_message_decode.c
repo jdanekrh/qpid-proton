c74b8ae1145eaa2720ad9afedad0f7abe0eabb93
Jiri Danek <jdanek@redhat.com>
2016-11-22 21:43:28 +0100

    few other attmpts at fuzzing, message and other stuff`

---
+++ proton-c/src/tests/fuzz_message_decode.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+#include "proton/message.h"
+
+// extern "C"
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    pn_message_t *msg = pn_message();
+    int ret = pn_message_decode(msg, (const char *) Data, Size);
+    if (ret == 0) {
+        // do something, like encode msg and compare again with Data
+    }
+    if (msg != NULL) {
+        pn_message_free(msg);
+    }
+    return 0;
+}
