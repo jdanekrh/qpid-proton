9a01d8b845373087973669943c0b94b52ec61e7d
Jiri Danek <jdanek@redhat.com>
2017-10-10 21:57:30 +0200

    Replace regular .travis.yml with one to upload to codecov.io

---
+++ bin/codecov.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cmake .. -DBUILD_GO=OFF -DCMAKE_BUILD_TYPE=Coverage
+make
+
+# list all tests
+ctest -N
+
+function run_and_report() {
+    local group=$1
+    local tests=$2
+    ctest -R "${tests}"
+    bash ../bin/record-coverage.sh . ..
+    bash <(curl -s https://codecov.io/bash) -F ${group}
+    find \( -name "*.gcov" -o -name "*.gcda" \) -exec rm {} \;
+}
+
+run_and_report "c" "c-object-tests|c-message-tests|c-engine-tests|c-parse-url-tests|c-refcount-tests|c-event-tests|c-data-tests|c-condition-tests|c-connection-driver-tests|c-proactor-tests|c-fdlimit-tests"
+run_and_report "cpp" "cpp-codec_test|cpp-connection_driver_test|cpp-interop_test|cpp-message_test|cpp-map_test|cpp-scalar_test|cpp-value_test|cpp-container_test|cpp-url_test|cpp-reconnect_test"
+run_and_report "python" "python-test"
+run_and_report "c-examples" "c-example-tests"
+run_and_report "cpp-examples" "cpp-example-container|cpp-example-container-ssl"
